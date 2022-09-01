befd16e7a52f316dd227de42aee62aece8b8dc86
msaidzengin <msaidzengin@gmail.com>
2022-09-01 14:37:22 +0300

    Change design

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-document.getElementById("search_button").onclick = function () {
-    var search_text = document.getElementById('choices-text-preset-values').value;
+document.getElementById("click_button").onclick = function () {
+    var search_text = document.getElementById('search').value;
     location.href = "program.html?kod=" + search_text;
 };
