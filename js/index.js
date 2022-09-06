24ff29a5998d367bad3c104c699df6239f8ff4e6
msaidzengin <msaidzengin@gmail.com>
2022-09-06 11:22:56 +0300

    Remove program page

--- js/index.js
+++ js/index.js
@@ -1,4 +1,8 @@
+backend_ip = "37.148.212.229"
+port = 9000;
+
 document.getElementById("click_button").onclick = function () {
-    var search_text = document.getElementById('search').value;
-    location.href = "program.html?kod=" + search_text;
+    var code = document.getElementById('search').value;
+    let url = "http://" + backend_ip + ":" + port + "/kullanici_programi_getir?kod=" + code;
+    window.location.replace(url);
 };
